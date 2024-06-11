b6acb306fbc713aee3c6a1bd00a7bebf7e5c6cda
The-Jimmy <166502543+The-Jimmy@users.noreply.github.com>
2024-06-11 22:56:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ def load_reranker():
     return FlagReranker('BAAI/bge-reranker-v2-m3', use_fp16=True)
 
 @st.cache_data
+def load_df(path):
+    df = pd.read_csv(path)
+    return df
+
+@st.cache_data
 def load_embed(path):
     embeddings_2 = np.load(path)
     return embeddings_2
@@ -20,17 +25,8 @@ def load_embed(path):
 model = load_model()
 reranker = load_reranker()
 
-embeddings_2 = load_embed('D:/AI_Builder/BGE_embeddings_2.npy')
-
-data = pd.DataFrame(pd.read_csv('D:/AI_Builder/ActualProject/DataCollection/TESTUNCLEANbookquestions.csv'))
-data2 = pd.DataFrame(pd.read_csv('D:/AI_Builder/ActualProject/DataCollection/TRAINbookquestions.csv'))
-data3 = pd.read_csv("D:/AI_Builder/ActualProject/DataCollection/booksummaries.txt",
-                              header=None,sep="\t",
-                              names=["ID", "Freebase ID", "Book Name", "Book Author", "Pub date", "Genres", "Summary"])
-df = pd.concat([data, data2])
-df = df.merge(data3, on='ID', how='left')
-df = df.rename(columns={'Book Name_x': 'Book Name'})
-df = df[['ID', 'Book Name', 'Book Author', 'Questions', 'Summary']]
+df = load_df('BookDataFrame.csv')
+embeddings_2 = load_embed('BGE_embeddings_2.npy')
 
 st.header(":books: Book Identifier")
 
